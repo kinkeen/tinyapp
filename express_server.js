3656600a5ce29b30ad8385ef0987be64ba91b7c0
kineret.braslavsky <kineret.braslavsky@gmail.com>
2021-01-20 21:50:15 +0000

    added routs for the shortURL

--- express_server.js
+++ express_server.js
@@ -10,8 +10,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+//.....app routes..........
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
@@ -19,19 +26,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
