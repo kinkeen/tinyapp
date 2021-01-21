86d0b435e3a3b09dbd2c1866e696b8a6929b4811
kineret.braslavsky <kineret.braslavsky@gmail.com>
2021-01-21 01:08:05 +0000

    add functionality of update url

--- express_server.js
+++ express_server.js
@@ -13,13 +13,19 @@ const urlDatabase = {
 
 
 //.....app routes..........
-app.get('/', (req, res) => {
-  res.send('Hello!')
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase )
 });
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -35,28 +41,31 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post('/urls', (req, res) => {
-  console.log(req.body)
-  res.send('received new url')
-})
-
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL] 
   res.redirect(longURL)
   
 })
 
-app.post('/urls', (req, res) => {
-  const shortURL = generateRandomString();
-  res.redirect(`/u/${shortURL}`)
-  urlDatabase[shortURL] = req.body.longURL;
-  console.log (urlDatabase);
-})
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
+});
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect('/urls')
 })
+
+app.post("/urls/:id", (req, res) => {
+  const templatedVars = {shortURL: req.params.id, longURL: urlDatabase[req.params.id] }
+  res.render('urls_show', templatedVars)
+  if (Object.keys(urlDatabase).includes(req.params.id)) {
+    const shortURL = req.params.id;
+    urlDatabase[shortURL] = req.body.newURL;
+  } 
+
+});
 
 //-----listen----------
 app.listen(PORT, () => {
